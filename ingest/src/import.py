49bf06443742ae53b56b7bf54a9748678791c0f5
Lee Roland <glroland@hotmail.com>
2025-10-26 11:29:33 -0400

    content ingestion through the openai is working

--- ingest/src/import.py
+++ ingest/src/import.py
@@ -2,19 +2,27 @@
 import os
 import sys
 import logging
+from io import BytesIO
 import click
-from llama_stack_client import LlamaStackClient, LlamaStackClient, RAGDocument
+from openai import OpenAI
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
 logger = logging.getLogger(__name__)
 
 MECHANIC_VECTOR_DB_NAME = "mechanic_vector_db"
 
+ENV_OPENAI_API_KEY = "OPENAI_API_KEY"
+
+DEFAULT_TIMEOUT = 30 * 60
+
+VECTOR_STORE_PROVIDER = "milvus-local"  # milvus-remote, milvus-local, faiss
+
 class ErrorCodes:
     SUCCESS = 0
     ILLEGAL_ARGS = 1
     FILE_NOT_FOUND = 2
     LLS_CONFIG_ERROR = 3
+    NO_API_KEY = 4
 
 class ColorOutputFormatter(logging.Formatter):
     """ Add colors to stdout logging output to simplify text.  Thank you to:
@@ -42,14 +50,14 @@ class ColorOutputFormatter(logging.Formatter):
         return formatter.format(record)
 
 @click.command()
-@click.argument('llama_stack_url')
+@click.argument('openai_baseurl')
 @click.argument('embedding_model_name')
 @click.argument('vdb_provider')
 @click.argument('input_file')
-def cli(llama_stack_url: str, embedding_model_name: str, vdb_provider:str, input_file: str):
+def cli(openai_baseurl: str, embedding_model_name: str, vdb_provider:str, input_file: str):
     """ CLI for importing mechanic content into vector store.
     
-        llama_stack_url - LLama Stack URL
+        openai_baseurl - OpenAI Base URL
         embedding_model_name - Embedding Model Name
         input_file - Input file to ingest
     """
@@ -63,8 +71,8 @@ def cli(llama_stack_url: str, embedding_model_name: str, vdb_provider:str, input
     logging.getLogger().addHandler(console)
 
     # Validate arguments
-    if llama_stack_url is None or len(llama_stack_url) == 0:
-        logger.fatal("LLama Stack URL is a required parameter and cannot be empty.")
+    if openai_baseurl is None or len(openai_baseurl) == 0:
+        logger.fatal("OpenAI Base URL is a required parameter and cannot be empty.")
         sys.exit(ErrorCodes.ILLEGAL_ARGS)
     if embedding_model_name is None or len(embedding_model_name) == 0:
         logger.fatal("Embedding Model is a required parameter and cannot be empty.")
@@ -84,12 +92,22 @@ def cli(llama_stack_url: str, embedding_model_name: str, vdb_provider:str, input
         logger.fatal("Input File is a directory.  This is currently unsupported functionality.  Filename = %s", input_file)
         sys.exit(ErrorCodes.FILE_NOT_FOUND)
 
+    # get openai key
+    apikey = None
+    if ENV_OPENAI_API_KEY in os.environ and len(os.environ[ENV_OPENAI_API_KEY]) > 0:
+        apikey = os.environ[ENV_OPENAI_API_KEY]
+    if apikey is None:
+        logger.fatal("OpenAI API Key not specified!")
+        sys.exit(ErrorCodes.NO_API_KEY)
+
     # Connect to LLama Stack
-    logger.info("Connecting to LLama Stack.  URL=%s", llama_stack_url)
-    llama_stack_client = LlamaStackClient(
-        base_url=llama_stack_url,
+    logger.info("Connecting to OpenAI Compatible Endpoint.  URL=%s", openai_baseurl)
+    openai_client = OpenAI(
+        base_url=openai_baseurl,
+        api_key=apikey,
+        timeout=DEFAULT_TIMEOUT
     )
-    logger.info("Successfully connected to LLama Stack.")
+    logger.info("Successfully connected to OpenAI Compatible Endpoint.")
 
     # Load the input file
     file_contents = ""
@@ -112,50 +130,54 @@ def cli(llama_stack_url: str, embedding_model_name: str, vdb_provider:str, input
     logger.info("File Chunked.  # of Chunks = %s", len(text_chunks))
 
     # Create a list of the registred Vector Databases
-    logger.info("Searching list of Vector DBs for pre-existing repositories.")
-    for vector_db in llama_stack_client.vector_dbs.list():
-        logger.debug("Vector DB vector_db_name = %s", vector_db.vector_db_name)
-        if vector_db.vector_db_name == MECHANIC_VECTOR_DB_NAME:
-            logger.warning("Preexisting instance of the vector database.  Deleting....  vector_db_id=%s", vector_db.identifier)
-            llama_stack_client.vector_dbs.unregister(vector_db.identifier)
+    vector_stores = openai_client.vector_stores.list()
+    logger.info("Searching list of Vector DBs for pre-existing repositories.  Pre-existing: %s", vector_stores)
+    for vector_db in vector_stores:
+        logger.debug("Vector DB in list.  ID=%s; Name=%s", vector_db.id, vector_db.name)
+        if vector_db.name == MECHANIC_VECTOR_DB_NAME:
+            logger.warning("Preexisting instance of the vector database.  Deleting....  id=%s", vector_db.id)
+            openai_client.vector_stores.delete(vector_store_id = vector_db.id)
 
     # Register a vector database
     logger.info("Creating new Vector Database for content.  ID/Name=%s", MECHANIC_VECTOR_DB_NAME)
-    new_vdb = llama_stack_client.vector_dbs.register(
-        vector_db_id=MECHANIC_VECTOR_DB_NAME,
-        embedding_model=embedding_model_name,
-        embedding_dimension=384,
-        provider_id=vdb_provider,
+    new_vdb = openai_client.vector_stores.create(
+        name=MECHANIC_VECTOR_DB_NAME,
+        chunking_strategy={
+            'type': 'static',
+            #'static': {
+            #    'max_chunk_size_tokens': 1000,  # Set maximum chunk size to 1000 tokens
+            #    'chunk_overlap_tokens': 200     # Set chunk overlap to 200 tokens
+            #}
+        },
+        extra_body={
+            "provider_id": VECTOR_STORE_PROVIDER,
+#            "embedding_model": embedding_model_name,
+#            "embedding_dimension": 384
+        }
     )
-    logger.info("Vector Database Created.  New VDB ID = %s", new_vdb.identifier)
+    vector_store_id = new_vdb.id
+    logger.info("Vector Database Created.  New VDB ID = %s", vector_store_id)
 
-    # Create chunks into Vector Database
-    logger.info("Creating LLama Stack RAG Documents...")
-    documents = [
-        RAGDocument(
-            document_id=f"num-{i}",
-            content=f"{chunk}",
-            mime_type="text/plain",
-            metadata={},
+    # insert each chunk
+    for i, chunk in enumerate(text_chunks):
+        # upload chunk and insert into vector store
+        #logger.info("Chunk: %s", chunk)
+        chunk_str = str(chunk)
+        file_content_bytes = BytesIO(chunk_str.encode('utf-8'))
+        uploaded_file = openai_client.files.create(
+            file=(f"chunk-{i}.txt", file_content_bytes, "text/plain"),
+            purpose="assistants"
         )
-        for i, chunk in enumerate(text_chunks)
-    ]
-
-    # Chunk the chunks so the LLama Stack API doesn't time out
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
-
-    # Import content
-    logger.info("Inserting RAG Documents into Vector Store...")
-    for chunk in chunks(documents, 10):
-        logger.info("Inserting a chunk of chunks...")
-        llama_stack_client.tool_runtime.rag_tool.insert(
-            documents=chunk,
-            vector_db_id=new_vdb.identifier,
-            chunk_size_in_tokens=2048,
+        logger.info("Uploaded Chunk as File: %s", uploaded_file.id)
+        vector_store_file = openai_client.vector_stores.files.create(
+            vector_store_id=vector_store_id,
+            file_id=uploaded_file.id
         )
+        logger.info("Associated Uploaded File with Vector Store: %s", vector_store_file.id)
+
+    # List files in a vector store
+    vector_store_files = openai_client.vector_stores.files.list(vector_store_id=vector_store_id)
+    logger.info("Vector Store File List: %s", vector_store_files)
 
     # Successfully imported
     logger.info("Successfully imported content.")
